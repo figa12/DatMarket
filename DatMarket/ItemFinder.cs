5361e7b6fcf2ca5d8ce78d4c918d774d1116a6f8
figa12 <figa.figa@gmail.com>
2013-06-16 22:00:22 +0200

    parallelism

--- DatMarket/ItemFinder.cs
+++ DatMarket/ItemFinder.cs
@@ -8,9 +8,12 @@ namespace DatMarket
 {
     public static class ItemFinder
     {
+        public static List<FoundItem> foundItems;
+        public static int counter = 0;
+
         public static List<FoundItem> itemListFinder(int maxJumps, int shipSpace, double minSecurity, float maxPrice)
         {
-            List<FoundItem> foundItems = new List<FoundItem>();
+            foundItems = new List<FoundItem>();
 
             /*
              * Det her tager cirka 1.1 mil * 30 sekunder at genneføre. Jeg tænker vi sammenligner alle ordrer i sell og buy med hinanden.
@@ -18,24 +21,35 @@ namespace DatMarket
              * Derudover bliver den meget hurtigere når vi får vores Qty'er regnet ud og ikke bare sammenlignet forkert.
              * HELD OG FUCKING LYKKE FIGA!
              */
+            /*
             foreach (var buyOrder in Orders.BuyOrders)
             {
                 if (buyOrder.QtyAvailable * buyOrder.Volume >= shipSpace && buyOrder.QtyMinimum * buyOrder.Volume <= shipSpace && buyOrder.Price * buyOrder.QtyMinimum <= maxPrice)
                 {
-                    foreach (var sellOrder in Orders.SellOrders)
+                    //checkSellOrders(buyOrder, maxJumps, shipSpace, minSecurity, maxPrice);
+                }
+            }
+            */
+            Parallel.ForEach(Orders.BuyOrders, item => checkSellOrders(item, maxJumps, shipSpace, minSecurity, maxPrice));
+            return ItemFinder.foundItems.OrderByDescending(x => x.ProfitPercent).ToList();
+        }
+
+        private static void checkSellOrders(Order buyOrder, int maxJumps, int shipSpace, double minSecurity, float maxPrice)
+        {
+            ItemFinder.counter++;
+            if (buyOrder.QtyAvailable * buyOrder.Volume >= shipSpace && buyOrder.QtyMinimum * buyOrder.Volume <= shipSpace && buyOrder.Price * buyOrder.QtyMinimum <= maxPrice)
+            {
+                foreach (var sellOrder in Orders.SellOrders)
+                {
+                    if (buyOrder.TypeId == sellOrder.TypeId && sellOrder.QtyAvailable * sellOrder.Volume >= shipSpace && sellOrder.QtyMinimum * sellOrder.Price <= maxPrice && sellOrder.Price < buyOrder.Price)
                     {
-                        if (buyOrder.TypeId == sellOrder.TypeId && sellOrder.QtyAvailable * sellOrder.Volume >= shipSpace && sellOrder.QtyMinimum * sellOrder.Price <= maxPrice && sellOrder.Price < buyOrder.Price)
+                        if (JumpGraph.GetRoute(sellOrder.SolarsystemId, buyOrder.SolarsystemId, minSecurity).Jumps <= maxJumps)
                         {
-                            if (JumpGraph.GetRoute(sellOrder.SolarsystemId, buyOrder.SolarsystemId, minSecurity).Jumps <= maxJumps)
-                            {
-                                foundItems.Add(new FoundItem(buyOrder, sellOrder));
-                            }
+                            ItemFinder.foundItems.Add(new FoundItem(buyOrder, sellOrder));
                         }
                     }
                 }
             }
-
-            return foundItems.OrderByDescending(x => x.ProfitPercent).ToList();
         }
     }
 
